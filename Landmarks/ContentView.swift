3db0aff9d9e8f8db32f47ab3313a600368fb5e5d
Md. Saber Hossain <saber.hossain@monstar-lab.com.bd>
2020-06-02 11:47:55 +0600

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Md. Saber Hossain on 2/6/20.
+//  Copyright © 2020 Md. Saber Hossain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
